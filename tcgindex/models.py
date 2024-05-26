3fcaeb098e011760783bf77b7e1188e8b80b01b8
Simon Görtzen <simon.goertzen@gmail.com>
2024-05-26 23:06:51 +0200

    Added timestamp fields to the base model and extended it to other models.

---
+++ tcgindex/models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from sqlalchemy import DateTime, func, text
+from sqlmodel import Column, Field, SQLModel
+
+class TimestampModel(SQLModel):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime.datetime = Field(
+        sa_column_kwargs={
+            "server_default": text("CURRENT_TIMESTAMP"),
+        }
+    )
+    updated_at: datetime.datetime | None = Field(
+        sa_column=Column(DateTime(), onupdate=func.now())
+    )
+
+class Catalog(TimestampModel, table=True):
+    ...
+
+class Game(TimestampModel, table=True):
+    ...
+
+class Expansion(TimestampModel, table=True):
+    ...
+
+class Card(TimestampModel, table=True):
+    ...
+
+class Printing(TimestampModel, table=True):
+    ...
+
